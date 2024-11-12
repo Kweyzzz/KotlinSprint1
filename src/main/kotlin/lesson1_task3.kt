96917556e5b8ad79f1f67409d35b8c000b1c301c
Kweyzzz <amal.5555@mail.ru>
2024-11-12 22:36:40 +0300

    KS-1-3 space

--- src/main/kotlin/lesson1_task3.kt
+++ src/main/kotlin/lesson1_task3.kt
@@ -1,14 +1,14 @@
 package org.example
 
 fun main(){
-    val year = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
     println("Год полета - $year")
     println("Взлет - $hour часов")
     println("Взлет - $minute минут")
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     println("Посадка - $hour:$minute")
 
 
