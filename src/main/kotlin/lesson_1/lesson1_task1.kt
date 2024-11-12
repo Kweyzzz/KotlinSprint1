6604626a36860d0472518423a28a7ff5a9851378
Kweyzzz <amal.5555@mail.ru>
2024-11-12 21:39:59 +0300

    KS-1-1 create variables

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -2,5 +2,5 @@ package org.example.lesson_1
 
 fun main(){
     val numberOfOrders = 75
-    val customerAppreciationText = "Спасибо за вашу покупку! Мы ценим ваш выбор и рады обслужить вас. Ждём вас снова!"
+    val customerAppreciationText = "Спасибо за вашу покупку!  Ждём вас снова!"
 }
