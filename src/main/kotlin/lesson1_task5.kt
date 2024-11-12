13e4de8c897a8daaf63691bc7aa547e887562b21
Kweyzzz <amal.5555@mail.ru>
2024-11-12 23:00:03 +0300

    KS-1-4-data types

---
+++ src/main/kotlin/lesson1_task5.kt
@@ -0,0 +1,14 @@
+package org.example
+
+fun main() {
+
+    val totalSeconds: Int = 6480
+
+
+    val hours: Int = totalSeconds / 3600
+    val minutes: Int = (totalSeconds % 3600) / 60
+    val seconds: Int = totalSeconds % 60
+
+
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
+}
