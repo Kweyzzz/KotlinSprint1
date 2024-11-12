bf5f4a0719e7a78fde3fa304d54bfdb95a995dc5
Kweyzzz <amal.5555@mail.ru>
2024-11-12 22:05:57 +0300

    KS-1-2 new variables

---
+++ src/main/kotlin/lesson1_task3.kt
@@ -0,0 +1,16 @@
+package org.example
+
+fun main(){
+    val year = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+    println("Год полета - $year")
+    println("Взлет - $hour часов")
+    println("Взлет - $minute минут")
+    hour = 10
+    minute = 55
+    println("Посадка - $hour:$minute")
+
+
+
+}
