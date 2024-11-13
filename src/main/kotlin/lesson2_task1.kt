b3e06943f69f2d1de67ef3194431ec23b031c28f
Kweyzzz <amal.5555@mail.ru>
2024-11-13 18:39:49 +0300

    KS-2-1-GPA

---
+++ src/main/kotlin/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example
+
+fun main(){
+    val student1:Float = 3f
+    val student2:Float = 4f
+    val student3:Float = 3f
+    val student4:Float = 5f
+
+    var averagePoint:Float = (student1+student2+student3+student4)/4
+    println(String.format("%.2f",averagePoint))
+
+}
