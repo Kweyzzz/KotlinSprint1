d903b9001d8207fd593f8ef40d398ef329eddc30
Kweyzzz <amal.5555@mail.ru>
2024-11-14 13:04:52 +0300

    KS-1-4-new

--- src/main/kotlin/lesson1_task4.kt
+++ src/main/kotlin/lesson1_task4.kt
@@ -5,7 +5,7 @@ fun main(){
     val age: Byte = 27
     val partOfDay: Float = 0.075f
     val seconds: Int = 6480
-    val partOfYear: Double = 2.0547945205479453E-4
+    val partOfYear: Float = 2.0547945E-4f
     val apogee: Int = 327000
 
     println(length)
