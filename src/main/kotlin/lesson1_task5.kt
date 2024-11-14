c90413411c75695997f80f1ee04223f8f0855b3b
Kweyzzz <amal.5555@mail.ru>
2024-11-14 12:59:19 +0300

    KS-1-5 new

--- src/main/kotlin/lesson1_task5.kt
+++ src/main/kotlin/lesson1_task5.kt
@@ -3,12 +3,9 @@ package org.example
 fun main() {
 
     val totalSeconds: Int = 6480
-
-
+    val const = 60
     val hours: Int = totalSeconds / 3600
-    val minutes: Int = (totalSeconds % 3600) / 60
-    val seconds: Int = totalSeconds % 60
-
-
+    val minutes: Int = (totalSeconds % 3600) / const
+    val seconds: Int = totalSeconds % const
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
