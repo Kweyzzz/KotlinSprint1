d5857e10076805d6e3f901365961a8880656bb83
Kweyzzz <amal.5555@mail.ru>
2024-11-16 20:42:20 +0300

    KS-2-5

--- src/main/kotlin/lesson2_task5.kt
+++ src/main/kotlin/lesson2_task5.kt
@@ -1,11 +1,10 @@
 package org.example
 import kotlin.math.pow
 
-fun main(){
+fun main() {
     val contributionAmount: Double = 70_000.0
     val interestRate: Double = 16.7
-    val period:Int = 20
-
+    val period: Int = 20
     val depositAmount: Double = contributionAmount * (1 + (interestRate/100)).pow(period)
     println("%.3f".format(depositAmount))
 }
